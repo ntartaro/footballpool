d6217b909bded308ff95007335bfde6a70d2285a
Nick Tartaro <ntartaro@gmail.com>
2018-10-01 11:09:26 -0400

    c o m m i t

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router()
+const User = require("../models/User")
 
 router.get('/', (req, res) => {
     res.render('index')
+})
+
+router.post('/user', (req, res) => {
+    User.create({
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => {
+        res.redirect('/')})
 })
 
 module.exports = router
--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
 const User = new Schema ({
-    username: String,
+    email: String,
     password: String
 })
 
-module.exports = mongoose.model('Item', Item);
+module.exports = mongoose.model('User', User);
