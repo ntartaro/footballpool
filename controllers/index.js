adfda70666f99fdb545006e9bf57b7c87da67b12
Nick Tartaro <ntartaro@gmail.com>
2018-10-05 10:23:11 -0400

    weekly update form

--- controllers/index.js
+++ controllers/index.js
@@ -29,12 +29,12 @@ router.get('/leaderboard', (req, res) => {
     })
 })
 
-//LOGIN FORM
+//LOGIN PAGE
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
-//SIGNUP FORM
+//SIGNUP PAGE
 router.get('/signup', (req, res) => {
     res.render('signup')
 })
@@ -102,14 +102,14 @@ router.delete('/user/:username', (req, res) => {
     })
 })
 
-//UPDATE 
+//UPDATE PAGE
 router.get('/user/:username/picks/update', (req, res) => {
-    console.log('GET THE UPDATE')
     var username = req.params.username
-    res.render('users/update', { username } )
+    ThisWeeksGames.find().then(games => {
+        var thisWeeksGame = games[0]
+        res.render('users/update', { username, thisWeeksGame } )
+    })
 })
-
-
 //UPDATE BUTTON
 router.post('/user/:username/picks/update', (req, res) => {
     var username = req.params.username
@@ -136,15 +136,57 @@ router.post('/user/:username/picks/update', (req, res) => {
     })
 })
 
-router.get('/supersecretadminpage', (req, res) => {
+//WEEKLY PICK PAGE
+router.get('/thisweekspicks', (req, res) => {
     res.render('thisweekspicks')
 })
-
+//WEEKLY PICK UPDATE
 router.post('/supersecretadminpage', (req, res) => {
     ThisWeeksGames.create({
-        gameOneFavorite: req.body.gameOneFavorite
+        gameOneFavorite: req.body.gameOneFavorite,
+        gameOneUnderdog: req.body.gameOneUnderdog,
+        gameOneSpread: req.body.gameOneSpread,
+        gameTwoFavorite: req.body.gameTwoFavorite,
+        gameTwoUnderdog: req.body.gameTwoUnderdog,
+        gameTwoSpread: req.body.gameTwoSpread,
+        gameThreeFavorite: req.body.gameThreeFavorite,
+        gameThreeUnderdog: req.body.gameThreeUnderdog,
+        gameThreeSpread: req.body.gameThreeSpread,
+        gameFourFavorite: req.body.gameFourFavorite,
+        gameFourUnderdog: req.body.gameFourUnderdog,
+        gameFourSpread: req.body.gameFourSpread,
+        gameFiveFavorite: req.body.gameFiveFavorite,
+        gameFiveUnderdog: req.body.gameFiveUnderdog,
+        gameFiveSpread: req.body.gameFiveSpread,
+        gameSixFavorite: req.body.gameSixFavorite,
+        gameSixUnderdog: req.body.gameSixUnderdog,
+        gameSixSpread: req.body.gameSixSpread,
+        gameSevenFavorite: req.body.gameSevenFavorite,
+        gameSevenUnderdog: req.body.gameSevenUnderdog,
+        gameSevenSpread: req.body.gameSevenSpread,
+        gameEightFavorite: req.body.gameEightFavorite,
+        gameEightUnderdog: req.body.gameEightUnderdog,
+        gameEightSpread: req.body.gameEightSpread,
+        gameNineFavorite: req.body.gameNineFavorite,
+        gameNineUnderdog: req.body.gameNineUnderdog,
+        gameNineSpread: req.body.gameNineSpread,
+        gameTenFavorite: req.body.gameTenFavorite,
+        gameTenUnderdog: req.body.gameTenUnderdog,
+        gameTenSpread: req.body.gameTenSpread,
+        gameElevenFavorite: req.body.gameElevenFavorite,
+        gameElevenUnderdog: req.body.gameElevenUnderdog,
+        gameElevenSpread: req.body.gameElevenSpread,
+        gameTwelveFavorite: req.body.gameTwelveFavorite,
+        gameTwelveUnderdog: req.body.gameTwelveUnderdog,
+        gameTwelveSpread: req.body.gameTwelveSpread,
+        gameThirteenFavorite: req.body.gameThirteenFavorite,
+        gameThirteenUnderdog: req.body.gameThirteenUnderdog,
+        gameThirteenSpread: req.body.gameThirteenSpread,
+        gameFourteenFavorite: req.body.gameFourteenFavorite,
+        gameFourteenUnderdog: req.body.gameFourteenUnderdog,
+        gameFourteenSpread: req.body.gameFourteenSpread
     }).then(user => {
-        res.redirect(`/user/Nick`)
+        res.redirect('/user/Nick')
     })
 })
 
